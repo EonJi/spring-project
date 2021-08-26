3a07633dd635e988a8ce094ecb6b145c683a906f
kejbg777 <kejyc6002@gmail.com>
2021-08-26 23:40:44 +0900

    프로젝트 생성

---
+++ src/test/java/mozzi/springproject/SpringProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package mozzi.springproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
